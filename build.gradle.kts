dad44d9fa2c677456c9e905c6949f1f572406fb5
Orisland <756176532@qq.com>
2022-09-07 20:24:58 +0800

    fix FileLose

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = "1.5.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.11.1"
+    id("net.mamoe.mirai-console") version "2.12.0"
 }
 
 group = "org.orisland"
